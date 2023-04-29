3da21927e7e8b334d3cbb59fdb975b569ccd0a05
Ogunniyi Samuel <samuelkayode622@gmail.com>
2023-04-29 14:52:20 +0100

    update package.json

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './DrawerToggle.css';
-var drawerToggle = function (props) { return (React.createElement("div", { className: "DrawerToggle", onClick: props.clicked },
-    React.createElement("div", null),
-    React.createElement("div", null),
-    React.createElement("div", null))); };
-export default drawerToggle;
