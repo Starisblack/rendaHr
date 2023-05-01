3984ebf64d87a2f12c2fb652cdeda6d2a53f290c
Ogunniyi Samuel <samuelkayode622@gmail.com>
2023-05-01 18:11:48 +0100

    update package.json

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,21 +4,21 @@ import HomeHero from "./HomeHero/HomeHero";
 import Tabs from "../../components/Tabs";
 import HomeTestimonials from "./HomeTestimonials";
 import Join from "../../components/Join";
-import "./Home.css"
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="page home-page hero-bg pb-0">
       <HomeHero />
       <div className="bg-white">
-      <Brands />
-      <Tabs />
-      <div className="relative">
-        <div className="md:bg-feature-ellipse bg-no-repeat xl:h-[1034px] xl:w-[1034px] md:h-[720px] md:w-[720px] bg-[contain]  absolute z-40"></div>
-        <HomeTestimonials />
-      </div>
+        <Brands />
+        <Tabs />
+        <div className="relative">
+          <div className="md:bg-feature-ellipse bg-no-repeat xl:h-[1034px] xl:w-[1034px] md:h-[720px] md:w-[720px] bg-[contain]  absolute z-40"></div>
+          <HomeTestimonials />
+        </div>
 
-      <Join />
+        <Join />
       </div>
     </div>
   );
